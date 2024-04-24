53bcc427b00c84c209a1436f97c483181eeb5543
VincentCongDao <congdanhdao95@gmail.com>
2024-04-23 21:13:28 -0700

    Admin can able to view the products in their dashboard

--- app/admin/manage-orders/page.tsx
+++ app/admin/manage-orders/page.tsx
@@ -1,5 +1,22 @@
-const ManageOrders = () => {
-  return <div>Manage Orders</div>;
+import Container from "@/components/container";
+import ManageProductsClient from "../manage-products/manageproductsclient";
+import getProducts from "@/action/getProducts";
+import { getCurrentUser } from "@/action/getCurrentUser";
+import NullData from "@/components/NullData";
+
+const ManageOrders = async () => {
+  const products = await getProducts({ category: null });
+  const currentUser = await getCurrentUser();
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="You have no access to this order" />;
+  }
+  return (
+    <div className="pt-8">
+      <Container>
+        <ManageProductsClient products={products} />
+      </Container>
+    </div>
+  );
 };
 
 export default ManageOrders;
--- app/admin/manage-products/page.tsx
+++ app/admin/manage-products/page.tsx
@@ -1,5 +1,22 @@
-const ManageProducts = () => {
-  return <div>View Products</div>;
+import Container from "@/components/container";
+import ManageProductsClient from "../manage-products/manageproductsclient";
+import getProducts from "@/action/getProducts";
+import { getCurrentUser } from "@/action/getCurrentUser";
+import NullData from "@/components/NullData";
+
+const ManageProducts = async () => {
+  const products = await getProducts({ category: null });
+  const currentUser = await getCurrentUser();
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="You have no access to this order" />;
+  }
+  return (
+    <div className="pt-8">
+      <Container>
+        <ManageProductsClient products={products} />
+      </Container>
+    </div>
+  );
 };
 
 export default ManageProducts;
